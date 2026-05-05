10ea89a1dd83e217f212958110374e91c1319af7
Sameer Khan <khan.sameer301999@gmail.com>
2026-05-05 11:19:00 +0530

    ..

--- tests/prep.ts
+++ tests/prep.ts
@@ -197,3 +197,21 @@ function countChar(str: string, target: string): number {
 
 // Example
 console.log(countChar("hello", "o")); // 2
+
+
+
+
+// Find the occurrence (frequency) of letters in a string
+function countChars(str: string, target: string): number {
+    let count = 0;
+
+    for (const char of str) {
+        if (char === target) 
+        count++;
+    }
+
+    return count;
+}
+
+// Example
+console.log(countChar("hello", "o")); // 2
